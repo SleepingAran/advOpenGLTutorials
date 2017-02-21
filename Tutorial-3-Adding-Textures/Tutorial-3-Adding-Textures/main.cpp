d212f7b11646bf83641b4b0a19e9e521675eda58
SleepingAran <cheahpehao@gmail.com>
2017-02-21 12:14:34 +0800

    removed triangle, added soil dependency

--- Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/main.cpp
+++ Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/main.cpp
@@ -36,35 +36,16 @@ int main()
 	glViewport(0, 0, 800, 600);
 
 	//SOIL Starts
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
 	int width, height;
 	unsigned char* image = SOIL_load_image("testing.png", &width, &height, 0, SOIL_LOAD_RGB);
-
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
 	//SOIL Ends
-
-	//Draw triangle start
-	//Set each vertices of the triangle
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
-
-	//Setup pointer array and buffer
-	GLuint VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	//Bind the Vertex Array object first, then bind and set vertex buffer and attribute pointer
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	//Draw triangle ends
 
 	//prevent app from self quit
 	while (!glfwWindowShouldClose(window))
