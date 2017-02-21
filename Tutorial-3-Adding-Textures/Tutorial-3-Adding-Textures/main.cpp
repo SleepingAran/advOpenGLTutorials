85734432a76d74b98b3407d0dd3d8c42d72b8207
SleepingAran <cheahpehao@gmail.com>
2017-02-21 12:34:49 +0800

    removed color and texture binding.

--- Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/main.cpp
+++ Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/main.cpp
@@ -49,13 +49,14 @@ int main()
 	SOIL_free_image_data(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	//SOIL Ends
-	//Initiate triangle
-	//Set each vertices of the triangle
+
+	//Initiate rectangle
+	//Set each vertices of the rectangle
 	GLfloat vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.5f, -0.5f, 0.0f,  
+		0.5f, -0.5f, 0.0f,   
+		0.0f, 0.5f, 0.0f,   
 	};
 
 	//Setup pointer array and buffer
@@ -69,10 +70,9 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-	//Initiate triangle ends
+	//Initiate rectangle ends
 
 	//prevent app from self quit
 	while (!glfwWindowShouldClose(window))
