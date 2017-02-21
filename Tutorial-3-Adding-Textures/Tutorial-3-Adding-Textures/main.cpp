fbe8f8329036e8376119ceead3cacceed10ef358
SleepingAran <cheahpehao@gmail.com>
2017-02-21 11:54:42 +0800

    added initiatlization

--- Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/main.cpp
+++ Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/main.cpp
@@ -5,8 +5,30 @@
 #include <SOIL.h>
 
 using namespace std;
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-void main()
+int main()
 {
+	//Initialization starts
+	glfwInit();
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	//initialize GLFW window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Tutorial 3", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		//if the window is not created, exit with error
+		cout << "Failed to create GLFW Window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	//Initialization ends
+
+	//set callback function
+	glfwSetKeyCallback(window, key_callback);
 }
