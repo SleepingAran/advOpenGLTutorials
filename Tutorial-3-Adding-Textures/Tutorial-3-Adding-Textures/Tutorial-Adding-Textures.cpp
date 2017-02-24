da07240b1c1bcc7dbeab7be5420c5b768aba81e2
SleepingAran <cheahpehao@gmail.com>
2017-02-24 09:37:52 +0800

    added texture coordinate attribute

--- Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/Tutorial-Adding-Textures.cpp
+++ Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/Tutorial-Adding-Textures.cpp
@@ -133,17 +133,26 @@ void main()
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
 
-	//Bind the Vertex Array object first, then bind and set vertex buffer and attribute pointer
+	//Bind the Vertex Array object first, then 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Bind Element Array
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+
+	//bind and set vertex buffer and attribute pointer
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	//Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+
+	//Texture Coordinate attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glDisableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
