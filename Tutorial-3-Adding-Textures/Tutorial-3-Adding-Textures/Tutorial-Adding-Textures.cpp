92f0b92c1f5fcf990279b0f92ed888f857985015
SleepingAran <cheahpehao@gmail.com>
2017-02-24 09:34:55 +0800

    binded and create element buffer object

--- Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/Tutorial-Adding-Textures.cpp
+++ Tutorial-3-Adding-Textures/Tutorial-3-Adding-Textures/Tutorial-Adding-Textures.cpp
@@ -125,15 +125,20 @@ void main()
 		0,1,3,
 		1,2,3,
 	};
+
 	//Setup pointer array and buffer
-	GLuint VBO, VAO;
+	//Element Buffer Object for indexing
+	GLuint VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	//Bind the Vertex Array object first, then bind and set vertex buffer and attribute pointer
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
