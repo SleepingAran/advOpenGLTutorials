dd6e7e121c31633934fb3e993ea817681fa02417
SleepingAran <cheahpehao@gmail.com>
2017-03-24 10:24:44 +0800

    fixed bug

--- Lighting/Source.cpp
+++ Lighting/Source.cpp
@@ -162,7 +162,7 @@ void main()
 		lastFrame = currentFrame;
 		glfwPollEvents();
 		//clear screen
-		glClearColor(.0f, .0f, .0f, 1.0f);
+		glClearColor(.5f, .5f, .5f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
 		//Call shader
