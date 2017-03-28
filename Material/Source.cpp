599af5cbea4edd59b44708b3caa5a49ad9fec084
SleepingAran <cheahpehao@gmail.com>
2017-03-28 10:57:22 +0800

    added Material vs and frag

--- Material/Source.cpp
+++ Material/Source.cpp
@@ -18,12 +18,14 @@ using namespace std;
 //Include Shader header file
 #include "Shader.h"
 #include "camera.h"
+
 const GLint WIDTH = 800, HEIGHT = 600;
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
 bool keys[1024];
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
 void mouse_callback(GLFWwindow* window, double Xcurrentpos, double Ycurrentpos);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
@@ -74,7 +76,7 @@ void main()
 	//Initialization End
 	///////////////////////////////////////////////
 
-	Shader ourShader("diffuse.vs","diffuse.frag");
+	Shader ourShader("material.vs","material.frag");
 	Shader lampShader("lamp.vs", "lamp.frag");
 	/*GLfloat vertices[] =
 	{
